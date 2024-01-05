87ead8f4cf7f413b476c057d0de82d965f8941a8
nobel7761 <habiburnobel@gmail.com>
2024-01-06 03:46:20 +0600

    add reusable breadcrumb and google fonts

--- src/components/ui/Contents.tsx
+++ src/components/ui/Contents.tsx
@@ -1,10 +1,29 @@
 "use client";
 import { Layout } from "antd";
+import UMBreadcrumb from "./UMBreadcrumb";
 
 const { Content } = Layout;
 
 const Contents = ({ children }: { children: React.ReactNode }) => {
-  return <Content style={{ minHeight: "100vh" }}>{children}</Content>;
+  const base = "admin";
+
+  return (
+    <Content style={{ minHeight: "100vh", color: "black" }}>
+      <UMBreadcrumb
+        items={[
+          {
+            label: `${base}`,
+            link: `/${base}`,
+          },
+          {
+            label: "student",
+            link: `/${base}/student`,
+          },
+        ]}
+      />
+      {children}
+    </Content>
+  );
 };
 
 export default Contents;
