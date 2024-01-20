ad37700b1a72b72a1f78fecf767b9ba4584f634a
nobel7761 <habiburnobel@gmail.com>
2024-01-21 02:23:21 +0600

    create getDepartmentList api into departmentApi and implement it

--- src/app/(withlayout)/super_admin/department/page.tsx
+++ src/app/(withlayout)/super_admin/department/page.tsx
@@ -3,57 +3,84 @@
 import ActionBar from "@/components/ui/ActionBar";
 import UMBreadcrumb from "@/components/ui/UMBreadcrumb";
 import UMTable from "@/components/ui/UMTable";
+import { useDepartmentsQuery } from "@/redux/api/departmentApi";
 import { getUserInfo } from "@/services/auth.service";
 import { Button } from "antd";
 import Link from "next/link";
+import { DeleteOutlined, EditOutlined, EyeOutlined } from "@ant-design/icons";
+import { useState } from "react";
 
 const DepartmentPage = () => {
   const { role } = getUserInfo() as any;
 
+  /* 
+  ================ starts ================
+  we have done this for pagination purpose. once the data will be changed from frontend, it will again call the GET request 
+  */
+  const query: Record<string, any> = {};
+  const [size, setSize] = useState<number>(10);
+  const [page, setPage] = useState<number>(1);
+  const [sortBy, setSortBy] = useState<string>("");
+  const [sortOrder, setSortOrder] = useState<string>("");
+  query["limit"] = size;
+  query["page"] = page;
+  query["sortBy"] = sortBy;
+  query["sortOrder"] = sortOrder;
+
+  const { data, isLoading } = useDepartmentsQuery({ ...query });
+
+  /* 
+  ================ ends ================
+  */
+
+  const departments = data?.departments;
+  const meta = data?.meta;
+
   const columns = [
     {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
     },
     {
-      title: "Age",
-      dataIndex: "age",
-      key: "age",
-      sorter: (a: any, b: any) => a.age - b.age,
+      title: "CreatedAt",
+      dataIndex: "createdAt",
+      key: "createdAt",
+      // sorter: (a: any, b: any) => a.age - b.age,
+      sorter: true,
     },
     {
       title: "Action",
       render: function (data: any) {
         return (
-          <Button type="primary" danger onClick={() => console.log(data)}>
-            X
-          </Button>
+          <div style={{ display: "flex", gap: "5px", alignItems: "center" }}>
+            <EyeOutlined
+              onClick={() => console.log(data)}
+              style={{ fontSize: "20px" }}
+            />
+            <EditOutlined
+              onClick={() => console.log(data)}
+              style={{ color: "blue", fontSize: "18px" }}
+            />
+            <DeleteOutlined
+              onClick={() => console.log(data)}
+              style={{ color: "red", fontSize: "20px" }}
+            />
+          </div>
         );
       },
     },
   ];
 
-  const tableData = [
-    {
-      key: "1",
-      name: "John Brown",
-      age: 32,
-    },
-    {
-      key: "2",
-      name: "Jim Green",
-      age: 42,
-    },
-  ];
-
   const onPaginationChange = (page: number, pageSize: number) => {
-    console.log("page", page, "pageSize", pageSize);
+    setPage(page);
+    setSize(pageSize);
   };
 
   const onTableChange = (pagination: any, filter: any, sorter: any) => {
     const { order, field } = sorter;
-    console.log(order, field);
+    setSortBy(field as string);
+    setSortOrder(order === "ascend" ? "asc" : "desc");
   };
 
   return (
@@ -74,12 +101,12 @@ const DepartmentPage = () => {
       </ActionBar>
 
       <UMTable
-        loading={false}
+        loading={isLoading}
         columns={columns}
-        dataSource={tableData}
+        dataSource={departments}
         showPagination={true}
-        pageSize={5}
-        totalPages={10}
+        pageSize={size}
+        totalPages={meta?.total}
         showSizeChanger={true}
         onPaginationChange={onPaginationChange}
         onTableChange={onTableChange}
--- src/redux/api/departmentApi.ts
+++ src/redux/api/departmentApi.ts
@@ -1,3 +1,4 @@
+import { IDepartment, IMeta } from "@/types";
 import { tagTypes } from "../tag-types";
 import { baseApi } from "./baseApi";
 
@@ -5,15 +6,29 @@ const DEPARTMENT_URL = "/management-departments";
 
 export const departmentApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
+    departments: build.query({
+      query: (arg: Record<string, any>) => ({
+        url: `${DEPARTMENT_URL}`,
+        method: "GET",
+        params: arg,
+      }),
+      transformResponse: (response: IDepartment, meta: IMeta) => {
+        return {
+          departments: response,
+          meta,
+        };
+      },
+      providesTags: [tagTypes.department], //provide tags means when we request the data and then we will get the data, it will cache the data
+    }),
     addDepartment: build.mutation({
       query: (departmentData) => ({
-        url: `${DEPARTMENT_URL}`,
+        url: `${DEPARTMENT_URL}/create-management`,
         method: "POST",
         data: departmentData,
       }),
-      invalidatesTags: [tagTypes.department],
+      invalidatesTags: [tagTypes.department], //invalidate tags means, it will remove the previous cache data if we get the data by making a post request.
     }),
   }),
 });
 
-export const { useAddDepartmentMutation } = departmentApi;
+export const { useDepartmentsQuery, useAddDepartmentMutation } = departmentApi;
